fd0aabca336d936d42b28381b053477022c973ba
Yogesh Mahajan <meyogesh20@gmail.com>
2022-08-19 17:05:53 +0530

    First Build

---
+++ Employee.java
@@ -0,0 +1,10 @@
+public class Employee{
+
+	public static void main(String [] args){
+	
+	System.out.println("Employee Id: 001");
+	System.out.println("Employee Name: Yogesh Mahajan");
+	System.out.println("Employee City: Pune");
+	
+	}
+}
